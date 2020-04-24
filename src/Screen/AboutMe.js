37184f721d6ca1dfdb9ab437bc6ad3281eb63772
jhuang429 <jhuang9240@gmail.com>
2020-04-23 21:12:11 -0400

    updates to about page

--- src/Screen/AboutMe.js
+++ src/Screen/AboutMe.js
@@ -5,6 +5,14 @@ function AboutMe() {
   return (
     <div className="content">
       <h1 class="title">About</h1>
+      <div style={{display:"flex", flexDirection:"row"}}>
+        <p>
+          I graduated from Syracuse University in the Spring of 2013 with a dual major in Accounting and Finance. After working in multiple roles in finance, operations, and systems, I have a well rounded experience in collaberating with multiple teams
+
+      </p>
+
+        <img src={require('../assets/headshot.png')} />
+      </div>
     </div>
   )
 }
