185112bf51f15a158fe1159aa594691579f3457a
jhuang429 <jhuang9240@gmail.com>
2020-04-30 21:24:26 -0400

    build folder

---
+++ build/precache-manifest.dc37bdef585a216be8b44dc67224978e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57b6fab22d13d743c8a083fb6d07225c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2f42e0362f3416c96ea",
+    "url": "/static/css/main.e3023f13.chunk.css"
+  },
+  {
+    "revision": "8ca0ffa4857f594edde9",
+    "url": "/static/js/2.594ccc1c.chunk.js"
+  },
+  {
+    "revision": "277f9ac65ea629533c781a2e9dd27b69",
+    "url": "/static/js/2.594ccc1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2f42e0362f3416c96ea",
+    "url": "/static/js/main.8fd71ad9.chunk.js"
+  },
+  {
+    "revision": "37c84a33fd6d0fe144d0",
+    "url": "/static/js/runtime-main.e4516502.js"
+  },
+  {
+    "revision": "14b6653fdebf7a442e9122ec46a18913",
+    "url": "/static/media/HVD Fonts - BrandonText-Light.14b6653f.woff"
+  },
+  {
+    "revision": "c336ddf7dcade6416a007ae0a5d02d05",
+    "url": "/static/media/HVD Fonts - BrandonText-Medium.c336ddf7.woff"
+  },
+  {
+    "revision": "23bb43e19934a28bc994f6b44b9bf574",
+    "url": "/static/media/Jerry_Huang_Resume.23bb43e1.pdf"
+  },
+  {
+    "revision": "d07609a6201940e1a79907778659d9cd",
+    "url": "/static/media/collo.d07609a6.png"
+  },
+  {
+    "revision": "49c2b5b9a7c666ecdba6954e12a3255e",
+    "url": "/static/media/headshot.49c2b5b9.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a73b45284f1ecaa9125981d5b6a6bf52",
+    "url": "/static/media/travelfrog.a73b4528.png"
+  },
+  {
+    "revision": "10ce4b45a5d671ec185053b9d83a7f8a",
+    "url": "/static/media/wheelygoodfood.10ce4b45.png"
+  }
+]);
